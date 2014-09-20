bf762d0127b8a64c4ff6e7c97a23006d0fb9b63f
Sakto <daniel@danwebs.dk>
2014-09-20 17:25:01 +0200

    Stop fucking with me git

--- application/logs/2014/09/20.php
+++
@@ -1,7 +0,0 @@
-<?php defined('SYSPATH') OR die('No direct script access.'); ?>
-
-2014-09-20 17:21:45 --- EMERGENCY: Kohana_Exception [ 0 ]: A valid cookie salt is required. Please set Cookie::$salt in your bootstrap.php. For more information check the documentation ~ SYSPATH/classes/Kohana/Cookie.php [ 151 ] in /home/daniel/Web/www/cmstest/system/classes/Kohana/Cookie.php:67
-2014-09-20 17:21:45 --- DEBUG: #0 /home/daniel/Web/www/cmstest/system/classes/Kohana/Cookie.php(67): Kohana_Cookie::salt('session', NULL)
-#1 /home/daniel/Web/www/cmstest/system/classes/Kohana/Request.php(151): Kohana_Cookie::get('session')
-#2 /home/daniel/Web/www/cmstest/index.php(117): Kohana_Request::factory(true, Array, false)
-#3 {main} in /home/daniel/Web/www/cmstest/system/classes/Kohana/Cookie.php:67
