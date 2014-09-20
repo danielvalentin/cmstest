b79e385679978ea50f6a3a0309cd8a8d05bbd15a
Sakto <daniel@danwebs.dk>
2014-09-20 17:15:02 +0200

    Deleting wrongly pushed files

--- application/logs/2014/09/20.php
+++ application/logs/2014/09/20.php
@@ -37,3 +37,38 @@
 #1 /home/daniel/Web/www/cmstest/system/classes/Kohana/Request.php(151): Kohana_Cookie::get('session')
 #2 /home/daniel/Web/www/cmstest/index.php(117): Kohana_Request::factory(true, Array, false)
 #3 {main} in /home/daniel/Web/www/cmstest/system/classes/Kohana/Cookie.php:67
+2014-09-20 17:08:45 --- EMERGENCY: Kohana_Exception [ 0 ]: A valid cookie salt is required. Please set Cookie::$salt in your bootstrap.php. For more information check the documentation ~ SYSPATH/classes/Kohana/Cookie.php [ 151 ] in /home/daniel/Web/www/cmstest/system/classes/Kohana/Cookie.php:67
+2014-09-20 17:08:45 --- DEBUG: #0 /home/daniel/Web/www/cmstest/system/classes/Kohana/Cookie.php(67): Kohana_Cookie::salt('session', NULL)
+#1 /home/daniel/Web/www/cmstest/system/classes/Kohana/Request.php(151): Kohana_Cookie::get('session')
+#2 /home/daniel/Web/www/cmstest/index.php(117): Kohana_Request::factory(true, Array, false)
+#3 {main} in /home/daniel/Web/www/cmstest/system/classes/Kohana/Cookie.php:67
+2014-09-20 17:09:45 --- EMERGENCY: Kohana_Exception [ 0 ]: A valid cookie salt is required. Please set Cookie::$salt in your bootstrap.php. For more information check the documentation ~ SYSPATH/classes/Kohana/Cookie.php [ 151 ] in /home/daniel/Web/www/cmstest/system/classes/Kohana/Cookie.php:67
+2014-09-20 17:09:45 --- DEBUG: #0 /home/daniel/Web/www/cmstest/system/classes/Kohana/Cookie.php(67): Kohana_Cookie::salt('session', NULL)
+#1 /home/daniel/Web/www/cmstest/system/classes/Kohana/Request.php(151): Kohana_Cookie::get('session')
+#2 /home/daniel/Web/www/cmstest/index.php(117): Kohana_Request::factory(true, Array, false)
+#3 {main} in /home/daniel/Web/www/cmstest/system/classes/Kohana/Cookie.php:67
+2014-09-20 17:10:45 --- EMERGENCY: Kohana_Exception [ 0 ]: A valid cookie salt is required. Please set Cookie::$salt in your bootstrap.php. For more information check the documentation ~ SYSPATH/classes/Kohana/Cookie.php [ 151 ] in /home/daniel/Web/www/cmstest/system/classes/Kohana/Cookie.php:67
+2014-09-20 17:10:45 --- DEBUG: #0 /home/daniel/Web/www/cmstest/system/classes/Kohana/Cookie.php(67): Kohana_Cookie::salt('session', NULL)
+#1 /home/daniel/Web/www/cmstest/system/classes/Kohana/Request.php(151): Kohana_Cookie::get('session')
+#2 /home/daniel/Web/www/cmstest/index.php(117): Kohana_Request::factory(true, Array, false)
+#3 {main} in /home/daniel/Web/www/cmstest/system/classes/Kohana/Cookie.php:67
+2014-09-20 17:11:45 --- EMERGENCY: Kohana_Exception [ 0 ]: A valid cookie salt is required. Please set Cookie::$salt in your bootstrap.php. For more information check the documentation ~ SYSPATH/classes/Kohana/Cookie.php [ 151 ] in /home/daniel/Web/www/cmstest/system/classes/Kohana/Cookie.php:67
+2014-09-20 17:11:45 --- DEBUG: #0 /home/daniel/Web/www/cmstest/system/classes/Kohana/Cookie.php(67): Kohana_Cookie::salt('session', NULL)
+#1 /home/daniel/Web/www/cmstest/system/classes/Kohana/Request.php(151): Kohana_Cookie::get('session')
+#2 /home/daniel/Web/www/cmstest/index.php(117): Kohana_Request::factory(true, Array, false)
+#3 {main} in /home/daniel/Web/www/cmstest/system/classes/Kohana/Cookie.php:67
+2014-09-20 17:12:45 --- EMERGENCY: Kohana_Exception [ 0 ]: A valid cookie salt is required. Please set Cookie::$salt in your bootstrap.php. For more information check the documentation ~ SYSPATH/classes/Kohana/Cookie.php [ 151 ] in /home/daniel/Web/www/cmstest/system/classes/Kohana/Cookie.php:67
+2014-09-20 17:12:45 --- DEBUG: #0 /home/daniel/Web/www/cmstest/system/classes/Kohana/Cookie.php(67): Kohana_Cookie::salt('session', NULL)
+#1 /home/daniel/Web/www/cmstest/system/classes/Kohana/Request.php(151): Kohana_Cookie::get('session')
+#2 /home/daniel/Web/www/cmstest/index.php(117): Kohana_Request::factory(true, Array, false)
+#3 {main} in /home/daniel/Web/www/cmstest/system/classes/Kohana/Cookie.php:67
+2014-09-20 17:13:45 --- EMERGENCY: Kohana_Exception [ 0 ]: A valid cookie salt is required. Please set Cookie::$salt in your bootstrap.php. For more information check the documentation ~ SYSPATH/classes/Kohana/Cookie.php [ 151 ] in /home/daniel/Web/www/cmstest/system/classes/Kohana/Cookie.php:67
+2014-09-20 17:13:45 --- DEBUG: #0 /home/daniel/Web/www/cmstest/system/classes/Kohana/Cookie.php(67): Kohana_Cookie::salt('session', NULL)
+#1 /home/daniel/Web/www/cmstest/system/classes/Kohana/Request.php(151): Kohana_Cookie::get('session')
+#2 /home/daniel/Web/www/cmstest/index.php(117): Kohana_Request::factory(true, Array, false)
+#3 {main} in /home/daniel/Web/www/cmstest/system/classes/Kohana/Cookie.php:67
+2014-09-20 17:14:45 --- EMERGENCY: Kohana_Exception [ 0 ]: A valid cookie salt is required. Please set Cookie::$salt in your bootstrap.php. For more information check the documentation ~ SYSPATH/classes/Kohana/Cookie.php [ 151 ] in /home/daniel/Web/www/cmstest/system/classes/Kohana/Cookie.php:67
+2014-09-20 17:14:45 --- DEBUG: #0 /home/daniel/Web/www/cmstest/system/classes/Kohana/Cookie.php(67): Kohana_Cookie::salt('session', NULL)
+#1 /home/daniel/Web/www/cmstest/system/classes/Kohana/Request.php(151): Kohana_Cookie::get('session')
+#2 /home/daniel/Web/www/cmstest/index.php(117): Kohana_Request::factory(true, Array, false)
+#3 {main} in /home/daniel/Web/www/cmstest/system/classes/Kohana/Cookie.php:67
