f77fe90d7cd579bfa9dbcefa5d673e83fa6d0dd4
Sakto <daniel@danwebs.dk>
2014-09-20 17:20:35 +0200

    Deleted unnecessary logs

--- application/logs/2014/05/30.php
+++
@@ -1,5 +0,0 @@
-<?php defined('SYSPATH') OR die('No direct script access.'); ?>
-
-2014-05-30 11:44:30 --- EMERGENCY: ErrorException [ 1 ]: Class 'Model_Vacation' not found ~ MODPATH/orm/classes/Kohana/ORM.php [ 46 ] in file:line
-2014-05-30 11:44:30 --- DEBUG: #0 [internal function]: Kohana_Core::shutdown_handler()
-#1 {main} in file:line
